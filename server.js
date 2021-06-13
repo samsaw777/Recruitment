9d54bd54faca703c5648dc1be0ad2b2a08e9a23a
Sameep <sameepsawant10@gmail.com>
2021-06-13 13:08:40 +0530

    resolve added

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post("/", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("mernfront/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "mernfront", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "mernfront", "build", "index.html"));
   });
 }
 app.listen(PORT, () => console.log(`Listening to port 9000`));
